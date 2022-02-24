5622834602cb9ea737f0532f2db310828795ac6c
EndoYuuki <shuckle.rbtech@gmail.com>
2022-02-24 13:09:27 +0900

    [update] jacobian_computation

--- scripts/jacobian_computation.py
+++ scripts/jacobian_computation.py
@@ -3,7 +3,7 @@ from torch.autograd.functional import jacobian
 
 if __name__ == '__main__':
     A = torch.tensor([[2.0, 0.0], [0.0, 0.5]])
-    x = torch.tensor([1.0, 2.0], requires_grad=True)
+    x = torch.tensor([1.0, 2.0])
 
     matmul_A = lambda x: torch.matmul(A, x)
 
